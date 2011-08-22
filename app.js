abffbc814c24d83377c0711c1bae173f92a628af
Damon Oehlman <damon.oehlman@gmail.com>
2011-08-22 13:14:18 +1000

    Updated test app with dependency on an external lib

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ module.exports = {
     _id: 'test',
     
     routes: [
-        '/loader => main.loader',
-        '/weather/:state/:city => weather.byCity',
-        '/weather/:state => weather.byState',
-        '/config => decarta.getConfig',
-        '/geocode/:address => decarta.geocode',
-        '/route/:waypoints => decarta.route'
+        '/time => test.getTime',
+        
+        {
+            path: '/counter',
+            handler: 'test.getCount'
+        },
+        
+        { 
+            path: '/html-test',
+            handler: 'test.genHTML'
+        }
     ]
 };
