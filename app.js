ca9d1cd5b4e4cd6fd1f9a3dd3901f7f04a79a08c
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-16 18:05:44 +1000

    Remove redundant code test app

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 module.exports = {
-    _id: 'test',
-    
-    routes: [
-        '/time => test.getTime',
-        
-        {
-            path: '/counter',
-            handler: 'test.getCount'
-        },
-        
-        { 
-            path: '/html-test',
-            handler: 'test.genHTML'
-        }
-    ]
+  _id: 'test',
+
+  routes: [
+    '/time => test.getTime',
+
+    {
+      path: '/counter',
+      handler: 'test.getCount'
+    },
+
+    {
+      path: '/html-test',
+      handler: 'test.genHTML'
+    }
+  ]
 };
