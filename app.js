c03f650bec5bb8196e78dd23756f63379ffa2af2
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-19 09:42:05 +1000

    Separated routes into routes.txt file

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-module.exports = {
-  _id: 'test',
-
-  routes: [
-    '/time => test.getTime',
-
-    {
-      path: '/counter',
-      handler: 'test.getCount'
-    },
-
-    {
-      path: '/html-test',
-      handler: 'test.genHTML'
-    }
-  ]
-};
+exports._id = 'test';
